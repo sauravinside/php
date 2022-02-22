e08a984d3d682fab653ede0ff510686ee6b86eea
Saurav Singh <63541034+sauravinside@users.noreply.github.com>
2022-02-22 13:44:22 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <body bgcolor="pink">
+
+    <?php
+    $x = 1;
+
+    while($x <= 5) {
+        echo "The number is: $x <br>";
+        $x++;
+    }
+    ?>
+
+</body>
+</html>
